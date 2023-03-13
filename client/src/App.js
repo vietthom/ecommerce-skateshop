b7e7864c4a26f451ef4aa559389f126bd80b7b16
vietthom <thomle0418@gmail.com>
2023-03-12 21:21:17 -0600

    Established routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,9 @@ import {
 } from 'react-router-dom';
 
 import Home from './scenes/home/Home';
+import ItemDetails from './scenes/itemDetails/ItemDetails';
+import Checkout from './scenes/checkout/Checkout';
+import Confirmation from './scenes/checkout/Confirmation';
 
 // ScrollToTop comes from react router
 const ScrollToTop = () =>{
@@ -26,6 +29,9 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
     </div>
